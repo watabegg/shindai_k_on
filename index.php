9915be579fae7498aaae3291f6321ff404b2d515
watabegg <roy_wtnb@outlook.jp>
2022-10-01 04:58:22 +0900

    Update
    コマカレンダーにMySQLからのデータの表示を実装

--- index.php
+++ index.php
@@ -25,22 +25,33 @@
     $weekterm = getAnyDay($Y_m_d, 0, 'Y年m月d日').'~' . getAnyDay($Y_m_d, 6, 'Y年m月d日');
 
     $table_box = []; //テーブルの中身を二次元配列で管理しちゃうぞ～！
+    $table_box2 = [];
+    for($i=0; $i < count($time); $i++){
+        $table_box[$i] = [];
+        $table_box2[$i] = [];
+        for($j=0; $j < 7; $j++){
+            $table_day = getAnyDay($Y_m_d, $j, 'Y-m-d');
+            $table_box[$i][$table_day] = null;
+            $table_box2[$i][$table_day] = null;
+        }
+    }
 
     $toweekSun = getAnyDay($Y_m_d, 0, 'Y-m-d');
     $toweekSat = getAnyDay($Y_m_d, 6, 'Y-m-d');
 
     try{
         $dbh = new PDO($localdsn, $localuser, $localpass);
-        $res = $dbh->query("SELECT day, time, regist_name, part, otherpart FROM booking WHERE day >= '$toweekSun' AND day <= '$toweekSat'");
+        $res = $dbh->query("SELECT booking_id, day, time, regist_name FROM booking WHERE day >= '$toweekSun' AND day <= '$toweekSat'");
         $date = $res->fetchAll(PDO::FETCH_ASSOC);
+        $count = $res->rowCount();
 
-        for($i=0; $i < 7; $i++){
-            $daydate = getAnyDay($toweekSun, $i, 'Y-m-d');
-            for($j=0; $j < count($time); $j++){
-                $table_box[$i][$j] =<<<_HTML_
-
-                _HTML_;
-            }
+        for($i=0; $i < $count; $i++){
+            $table_box[$date[$i]["time"]][$date[$i]["day"]] .= $date[$i]["booking_id"] . ',';
+            $table_box2[$date[$i]["time"]][$date[$i]["day"]] .= $date[$i]["regist_name"] . ',';
+        }
+        for($i=0; $i < $count; $i++){
+            $table_box[$date[$i]["time"]][$date[$i]["day"]] = rtrim($table_box[$date[$i]["time"]][$date[$i]["day"]], ",");
+            $table_box2[$date[$i]["time"]][$date[$i]["day"]] = rtrim($table_box2[$date[$i]["time"]][$date[$i]["day"]], ",");
         }
     }
     catch(PDOException $e){
@@ -56,18 +67,121 @@
     $table .= '</tr>';
     for($i = 0; $i < count($time); $i++) {
         $table .= '<tr><th class="'. $row_style[$i % 2] . ' table_time">' .$time[$i].'</th>';
-        for($k = 0; $k < 7; $k++) {
-            list($y, $m, $d) = getAnyDay($Y_m_d, $k, 'split');
-            $hyt = getAnyDay($Y_m_d, $k, 'Y-m-d');
-            if(isBorA($y, $m, $d) == 'today'){
-                $table .= '<td class="today"><a class="booking_window" href="./booking/index.html?day='.$hyt.'&num='.$i.'">' .'×<br>予約を確認'.'</a></td>';
+        for($j = 0; $j < 7; $j++) {
+            list($y, $m, $d) = getAnyDay($Y_m_d, $j, 'split');
+            $theday = getAnyDay($Y_m_d, $j, 'Y-m-d');
+            $table_class = isBorA($y, $m, $d);
+            if(mb_strlen($table_box[$i][$theday]) > 17){
+                $id_some = explode(",", $table_box[$i][$theday]);
+                $part_some = [];
+                for($k=0; $k < count($id_some); $k++){
+                    $part_some[$k] = intval(substr($id_some[$k], -7, -1));
+                }
+                if(array_product($part_some) == 510510){
+                    $table .= <<<_HTML_
+                        <td class="$table_class">
+                            <div class="table_box">
+                            <a class="booking_window" href="./confirm/index.html?day=$theday&num=$i">
+                                <div class="table_Symbol">
+                                    個人練習
+                                </div>
+                                <div class="table_some">
+                                    <span class="table_detail">詳細</span>
+                                </div>
+                            </a>
+                            </div>
+                        </td>
+                    _HTML_;
+                }
+                else{
+                    $table .= <<<_HTML_
+                        <td class="$table_class">
+                            <div class="table_box">
+                            <a class="booking_window" href="./booking/index.html?day=$theday&num=$i">
+                                <div class="table_Symbol">
+                                    <span class="material-symbols-outlined">pentagon</span>
+                                </div>
+                                <div class="table_some">
+                                    <span class="table_booking">予約</span>
+                                    <span class="table_detail"><a href="./confirm/index.html?day=$theday&num=$i">詳細</a></span>
+                                </div>
+                            </a>
+                            </div>
+                        </td>
+                    _HTML_;
+                }
             }
-            elseif(isBorA($y, $m, $d) == 'Past'){
-                $table .= '<td class="Past">'.'〇<br>予約する'.'</td>';
+            elseif(mb_strlen($table_box[$i][$theday]) == 0){
+                $table .= <<<_HTML_
+                    <td class="$table_class">
+                        <div class="table_box">
+                        <a class="booking_window" href="./booking/index.html?day=$theday&num=$i">
+                            <div class="table_Symbol">
+                                <span class="material-symbols-outlined">circle</span>
+                            </div>
+                            <div class="table_some">
+                                <span class="table_booking">予約</span>
+                            </div>
+                        </a>
+                        </div>
+                    </td>
+                _HTML_;
             }
-            elseif(isBorA($y, $m, $d) == 'Future'){
-                $table .= '<td class="Future"><a class="booking_window" href="./booking/index.html?day='.$hyt.'&num='.$i.'">'.'△<br>予約する,予約を確認'.'</a></td>';
+            else{
+                if(substr($table_box[$i][$theday], -7, -1) == "510510"){
+
+                    $table .= <<<_HTML_
+                        <td class="$table_class">
+                            <div class="table_box">
+                            <a class="booking_window" href="./confirm/index.html?day=$theday&num=$i">
+                                <div class="table_Symbol">
+                                    {$table_box2[$i][$theday]}
+                                </div>
+                                <div class="table_some">
+                                    <span class="table_detail">詳細</span>
+                                </div>
+                            </a>
+                            </div>
+                        </td>
+                    _HTML_;
+                }
+                else{
+                    if(substr($table_box[$i][$theday], 16, 17) == "1"){
+                        $table .= <<<_HTML_
+                            <td class="$table_class">
+                                <div class="table_box">
+                                <a class="booking_window" href="./booking/index.html?day=$theday&num=$i">
+                                    <div class="table_Symbol">
+                                        <span class="material-symbols-outlined">pentagon</span>
+                                    </div>
+                                    <div class="table_some">
+                                        <span class="table_booking">予約</span>
+                                        <span class="table_detail"><a href="./confirm/index.html?day=$theday&num=$i">詳細</a></span>
+                                    </div>
+                                </a>
+                                </div>
+                            </td>
+                        _HTML_;
+                    }
+                    elseif(substr($table_box[$i][$theday], 16, 17) == "0"){
+                        $table .= <<<_HTML_
+                            <td class="$table_class">
+                                <div class="table_box">
+                                <a class="booking_window" href="./confirm/index.html?day=$theday&num=$i">
+                                    <div class="table_Symbol">
+                                        個人練習
+                                    </div>
+                                    <div class="table_some">
+                                        <span class="table_detail">詳細</span>
+                                    </div>
+                                </a>
+                                </div>
+                            </td>
+                        _HTML_;
+                    }
+                }
             }
+            //$table .= '<td class="today"><a class="booking_window" href="./booking/index.html?day='.$theday.'&num='.$i.'">' .'×<br>予約を確認'.'</a></td>';
         }
         $table .= '</tr>';
     }
